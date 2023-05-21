c3c46bca6aaf21eb6ff8cb3c81592f2853136e58
david-250 <hsayndev123@gmail.com>
2023-05-21 14:15:46 +0300

    Adding models file with classes

---
+++ PPT/Models/Attendance.cs
@@ -0,0 +1,9 @@
+﻿namespace PPT.Models
+{
+    public class Attendance
+    {
+        int id { get; set; }
+        DateTime date { get; set; }
+        int duration { get; set; } // default 0 , this is only for contract dr
+    }
+}
---
+++ PPT/Models/Faculty.cs
@@ -0,0 +1,8 @@
+﻿namespace PPT.Models
+{
+    public class Faculty
+    {
+        int id { get; set; }
+        string name { get; set; }
+    }
+}
